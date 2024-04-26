54ee9a9123dc6ef904635eda8266fb03a33552e3
Ahmed Basuony <A.Basuony.social@gmail.com>
2024-04-26 20:21:08 +0300

    "Change MealsPage to fetch and display meals from an API"
    Explanation:
    
    * The `MealsPage` component has been changed from a synchronous function to an asynchronous one.
    * The `get

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
         <h1>
           Delicious meals, created
-          <span className={classes.highlight}>by you</span>
+          <span className={classes.highlight}> by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun
@@ -19,7 +21,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
